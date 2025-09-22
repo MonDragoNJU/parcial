b4188fe14f209fa97717e8f8830945536de79af6
Julian Mondragon <j.mondragon1@uniandes.edu.co>
2025-09-22 17:59:24 -0500

    Pruebas con Toaster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+"use client";
+
+import { toast, Toaster } from "sonner";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="red">ESTE ES MI PARCIAL SIU</h1>
-    </div>
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center p-24">
+        <button
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          onClick={() => toast("Hello, world!")}
+        >
+          Show Toast
+        </button>
+      </div>
+      <Toaster />
+    </>
   );
 }
